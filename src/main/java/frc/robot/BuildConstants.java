28ff0012bed169b99c85834a9f0c9f6f80d3a4ea
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2025-12-12 17:25:39 -0800

    added some gradle flags for garbage oh yeah advantage scope breaks and three other things break when run in parallel

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "arc-generic";
+  public static final String MAVEN_NAME = "arc-template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "b77d3aa834835f85af71ed57003a39f040cdd273";
-  public static final String GIT_DATE = "2025-11-14 15:41:59 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-11-14 20:46:59 EST";
-  public static final long BUILD_UNIX_TIME = 1763171219229L;
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "b564208f1ed11057cab2caec4426ddca6bd28e29";
+  public static final String GIT_DATE = "2025-11-14 23:16:06 EST";
+  public static final String GIT_BRANCH = "ctre-swerve";
+  public static final String BUILD_DATE = "2025-12-12 20:22:57 EST";
+  public static final long BUILD_UNIX_TIME = 1765588977998L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
