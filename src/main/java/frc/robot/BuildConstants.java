1745002f6acd094203f53545e6fd2d19bd0310a1
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2026-01-12 17:32:50 -0800

    Set to joystickdriveatangle()

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "arc-template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "a4ae3f6e74df2e44f4749398d70b47daa8a17d43";
-  public static final String GIT_DATE = "2026-01-09 17:41:24 EST";
-  public static final String GIT_BRANCH = "ctre-swerve";
-  public static final String BUILD_DATE = "2026-01-11 14:50:28 EST";
-  public static final long BUILD_UNIX_TIME = 1768161028198L;
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "4216597f9470f101cdadcffe5b4fdab22165651f";
+  public static final String GIT_DATE = "2026-01-11 15:18:31 EST";
+  public static final String GIT_BRANCH = "swerve-improvement";
+  public static final String BUILD_DATE = "2026-01-12 20:26:34 EST";
+  public static final long BUILD_UNIX_TIME = 1768267594161L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -127,11 +128,11 @@ public class RobotContainer {
     //         () -> -steerJoystick.getX()));
 
     drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
+        DriveCommands.joystickDriveAtAngle(
             drive,
             () -> driveJoystick.getY(),
             () -> driveJoystick.getX(),
-            () -> steerJoystick.getX()));
+            () -> new Rotation2d(-steerJoystick.getY(), -steerJoystick.getX())));
 
     // // Lock to 0° when A button is held
     // controller
