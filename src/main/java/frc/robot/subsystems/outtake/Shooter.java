4ffee5a9124018b703cc69e6fe2b289023915a0e
JS7077 <139284744+JS7077@users.noreply.github.com>
2026-01-30 20:55:07 -0800

    more unsaved things

--- src/main/java/frc/robot/subsystems/outtake/Shooter.java
+++ src/main/java/frc/robot/subsystems/outtake/Shooter.java
@@ -3,60 +3,70 @@ package frc.robot.subsystems.outtake;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase implements OuttakeIO {
-  private final TalonFX rightShooter, leftShooter, intake, angleChanger;
-  private final OuttakeIOLogs logs;
+import org.littletonrobotics.junction.Logger;
+
+import frc.robot.subsystems.outtake.OuttakeIO;
+import frc.robot.subsystems.outtake.OuttakeIO.OuttakeIOInputs;
+import frc.robot.Constants.OuttakeConstants;
+
+public class Shooter extends SubsystemBase {
+  /**This is the leader of the leftShooter. It's gearing is 144:1 */
+  private final TalonFX rightShooter;
+  /**This follows the right shooter */
+  private final TalonFX leftShooter;
+  private final TalonFX intake, angleChanger, starWheel;
+  private final OuttakeIO io;
+  private final OuttakeIOInputsAutoLogged logs;
 
   public Shooter() {
     super();
-    logs = new OuttakeIOLogs();
-    rightShooter = new TalonFX(-1);
-    leftShooter = new TalonFX(-1);
-    leftShooter.setControl(new Follower(-1, MotorAlignmentValue.Opposed));
-    intake = new TalonFX(-1);
-    angleChanger = new TalonFX(-1);
+    rightShooter = new TalonFX(OuttakeConstants.RIGHT_SHOOTER_ID);
+    leftShooter = new TalonFX(OuttakeConstants.LEFT_SHOOTER_ID);
+    leftShooter.setControl(new Follower(OuttakeConstants.RIGHT_SHOOTER_ID, MotorAlignmentValue.Opposed));
+    intake = new TalonFX(OuttakeConstants.INTAKE_MOTOR_ID);
+    angleChanger = new TalonFX(OuttakeConstants.ANGLE_CHANGER_ID);
+    starWheel = new TalonFX(OuttakeConstants.STAR_WHEEL_ID);
+    io = new OuttakeIOShooter(intake);
+    logs = new OuttakeIOInputsAutoLogged();
   }
 
   /**
-   * Sets the angle of the hood.
-   *
-   * @param degrees The number of degrees to turn to, from <x>º to <y>º
+   * Sets the angle of the hood. 
+   * @param degrees The number of degrees to turn to, from {@value OuttakeConstants#MIN_HOOD_ANGLE_DEGREES}º to {@value OuttakeConstants#MAX_HOOD_ANGLE_DEGREES}º 
    */
   public Command setHoodAngle(double degrees) {
-    // logs.hoodAngle.set(degrees);
+    degrees = Math.max(OuttakeConstants.MIN_HOOD_ANGLE_DEGREES, Math.min(OuttakeConstants.MAX_HOOD_ANGLE_DEGREES, degrees));
     double target = 0;
     return new InstantCommand(() -> angleChanger.setPosition(target), this);
   }
-
+  /**Sets the angle of the hood to {@link #calculateAngle} */
   public Command setHoodAngle() {
-    return setHoodAngle(0.0);
+    return setHoodAngle(calculateAngle());
   }
-
+  
   /**
-   * Calculates how to aim for the hub.
-   *
+   * Calculates how to aim for the hub. 
    * @return the angle required to shoot into the hub from its location
    */
   public double calculateAngle() {
-    return 0.0;
+    return new Saathvik().mechanics(new Pose2d());
   }
 
   public Command intake() {
-    return new StartEndCommand(() -> intake.set(0.1), () -> intake.set(0.0), this);
+    return new StartEndCommand(
+      () -> {intake.set(OuttakeConstants.INTAKE_SPEED); starWheel.set(OuttakeConstants.INTAKE_SPEED);}, 
+      () -> {intake.set(0); starWheel.set(0);}, 
+      this);
   }
 
-  public Command outtake(double speed) {
-    return null;
-  }
-
-  @Override
-  public void updateLogs(OuttakeIOLogs logs) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'updateLogs'");
+  public Command outtake() {
+    return new StartEndCommand(() -> rightShooter.set(OuttakeConstants.OUTTAKE_SPEED), () -> rightShooter.set(0), this);
   }
 }
