281f321a0aaeb256fc6bda574d98781733253d7a
JS7077 <139284744+JS7077@users.noreply.github.com>
2026-01-30 20:53:18 -0800

    some stuff didnt save

--- src/main/java/frc/robot/subsystems/outtake/OuttakeIO.java
+++ src/main/java/frc/robot/subsystems/outtake/OuttakeIO.java
@@ -1,24 +1,17 @@
 package frc.robot.subsystems.outtake;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import org.littletonrobotics.junction.AutoLog;
 
-public interface OuttakeIO extends Subsystem {
-  @AutoLog
-  public static class OuttakeIOLogs {
-    /** The current speed in rpm of the outtake motor */
-    public double outtakeMotorSpeed = 0.0;
-    /** */
-    public double outtakeMotorVoltage = 0.0;
+public interface OuttakeIO {
+    @AutoLog
+    public static class OuttakeIOInputs {
+        /**The current speed in radians per second of the outtake motor */
+        public double outtakeMotorRadPerSec = 0.0;
+        /**The current voltage of the outtake motor */
+        public double outtakeMotorVoltage = 0.0;
+        /**The current angle of the hood in degrees from the horizontal */
+        public double hoodAngle = 0.0;
+    }
 
-    public double hoodAngle = 0.0;
-  }
-
-  public final TalonFX outtakeMotor = null;
-
-  public Command outtake(double speed);
-
-  public void updateLogs(OuttakeIOLogs logs);
+    public void updateLogs(OuttakeIOInputs inputs);
 }
--- src/main/java/frc/robot/subsystems/outtake/OuttakeIOShooter.java
+++ src/main/java/frc/robot/subsystems/outtake/OuttakeIOShooter.java
@@ -1,1 +1,27 @@
+package frc.robot.subsystems.outtake;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+
+public class OuttakeIOShooter implements OuttakeIO {
+    private final StatusSignal<AngularVelocity> outtakeRotationSpeed;
+    private final StatusSignal<Voltage> outtakeAppliedVolts;
+
+    public OuttakeIOShooter(TalonFX outtakeMotor) {
+        outtakeRotationSpeed = outtakeMotor.getVelocity();
+        outtakeAppliedVolts = outtakeMotor.getMotorVoltage();
+    }
+
+    @Override
+    public void updateLogs(OuttakeIOInputs inputs) {
+        BaseStatusSignal.refreshAll(outtakeRotationSpeed,outtakeAppliedVolts);
+        inputs.outtakeMotorRadPerSec = outtakeRotationSpeed.getValueAsDouble();
+        inputs.outtakeMotorVoltage = outtakeAppliedVolts.getValueAsDouble();
+    }
+
+    
+}
