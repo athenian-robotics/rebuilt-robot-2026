ddb1a3ca722cf7b1e37b0f0864fe8aa742247efa
28dchen <28dchen@athenian.org>
2026-02-02 17:35:01 -0800

    Address code review comments
    - Revert build.gradle changes
    - Restore .gitmodules
    - Remove deprecated SmartDashboard logging from Drive.java

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -46,7 +46,6 @@ import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -165,7 +164,6 @@ public class Drive extends SubsystemBase {
             new SysIdRoutine.Mechanism(
                 (voltage) -> runCharacterization(voltage.in(Volts)), null, this));
 
-    SmartDashboard.putData("Field", field);
   }
 
   @Override
