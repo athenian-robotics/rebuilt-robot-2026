a75e702ceb359f8ee69e590ff63f93333fa1ed00
Saathvik Kooturu <kooturusaathvik@gmail.com>
2026-02-02 17:29:15 -0800

    figuring out simulation

---
+++ src/main/java/frc/robot/subsystems/Hopper/Hopper.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.Hopper;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+import frc.robot.Constants.HopperConstants;
+import frc.robot.subsystems.hopintake.HopperIOInputsAutoLogged;
+
+public class Hopper extends SubsystemBase{
+            
+    private HopperIO io;
+    private HopperIOInputsAutoLogged inputs = new HopperIOInputsAutoLogged();
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Hopper", inputs);
+    }
+
+    public void retractedToPartial(){
+        io.moveToLength(HopperConstants.SETPOINT_PARTIALLY_EXTENDED_INCHES);
+    }
+    public void partialToFull(){
+        io.moveToLength(HopperConstants.SETPOINT_EXTENDED_INCHES);
+    }
+    public void fullToRetracted(){
+        io.moveToLength(HopperConstants.SETPOINT_RETRACTED_INCHES);
+    }
+}
--- src/main/java/frc/robot/subsystems/Hopper/HopperIO.java
+++ src/main/java/frc/robot/subsystems/Hopper/HopperIO.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.hopintake;
 
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
 import org.littletonrobotics.junction.AutoLog;
@@ -10,6 +11,7 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.units.measure.Voltage;
 import frc.robot.Constants.HopperConstants;
 
@@ -17,8 +19,9 @@ public interface HopperIO {
     @AutoLog
     public static class HopperIOInputs{
         public Distance hopperExtension = Inches.of(0);
-        public Voltage hopperVoltage = Volts.of(0);
         public LinearVelocity hopperExtensionVelocity = InchesPerSecond.of(0);
+        public Voltage hopperVoltage = Volts.of(0);
+        public Amperage currentAmperage = Amps.of(0);
         public SimpleMotorFeedforward hopperFeedforward = new SimpleMotorFeedforward(HopperConstants.kS, HopperConstants.kV);
         
     }
