3b72dcb1ffde42f9d613d941a800f926b1a2ad11
JS7077 <139284744+JS7077@users.noreply.github.com>
2026-02-07 15:36:28 -0800

    Added topic subscribing, outtakeiosim

--- src/main/java/frc/robot/subsystems/outtake/Outtake.java
+++ src/main/java/frc/robot/subsystems/outtake/Outtake.java
@@ -1,9 +1,6 @@
 package frc.robot.subsystems.outtake;
 
-import static edu.wpi.first.units.Units.Volts;
-
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
---
+++ src/main/java/frc/robot/subsystems/outtake/OuttakeIOSim.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.outtake;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class OuttakeIOSim implements OuttakeIO {
+
+    @Override
+    public void updateInputs(OuttakeIOInputs inputs) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+    }
+
+    @Override
+    public void startFlywheel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startFlywheel'");
+    }
+
+    @Override
+    public void stopFlywheel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stopFlywheel'");
+    }
+
+    @Override
+    public void setMiddleWheelVoltage(double voltage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setMiddleWheelVoltage'");
+    }
+
+    @Override
+    public void setStarWheelVoltage(double voltage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStarWheelVoltage'");
+    }
+
+    @Override
+    public void setAngleAtTarget(Translation2d currentPosition) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setAngleAtTarget'");
+    }
+
+    @Override
+    public void setAngle(double angleDegrees) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setAngle'");
+    }
+    
+}
