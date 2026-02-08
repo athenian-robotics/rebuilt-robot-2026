268277cf055e844220d63d466c3fbbe6fdcb8d70
Saathvik Kooturu <kooturusaathvik@gmail.com>
2026-02-08 12:54:07 -0800

    changes to hopperSIM, might be working now

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 86;
-  public static final String GIT_SHA = "4e27c443082235aa19bdee0943ce694eb341e2fa";
-  public static final String GIT_DATE = "2026-02-08 14:21:37 EST";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "ec303d7a29d099532d6bc344748ac78812aaf18d";
+  public static final String GIT_DATE = "2026-02-08 14:44:46 EST";
   public static final String GIT_BRANCH = "HopperSIM";
-  public static final String BUILD_DATE = "2026-02-08 14:43:22 EST";
-  public static final long BUILD_UNIX_TIME = 1770579802586L;
+  public static final String BUILD_DATE = "2026-02-08 15:51:06 EST";
+  public static final long BUILD_UNIX_TIME = 1770583866812L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems.hopper;
 
-import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 
@@ -11,32 +13,30 @@ import frc.robot.Constants.HopperConstants;
 
 // TODO fully implement class
 public class HopperIOSim implements HopperIO {
-    //SparkMax motor = new SparkMax(HopperConstants.SPARK_ID, MotorType.kBrushless);
-    DCMotor m = DCMotor.getNeo550(1);
-    //motor.getClosedLoo
     
-   // private final SparkMaxSim hopperMotor = new SparkMaxSim(motor, m);
-   // private final SparkClosedLoopController pidController = motor.getClosedLoopController();
-
+    DCMotor m = DCMotor.getNeo550(1);
+    
+    
+    private final SparkMax hopperMotor = new SparkMax(HopperConstants.SPARK_ID, MotorType.kBrushless);
+    private final SparkMaxSim simMotor = new SparkMaxSim(hopperMotor, m);
+    private final SparkClosedLoopController pidController = hopperMotor.getClosedLoopController();
+    private final RelativeEncoder hopperEncoder = hopperMotor.getEncoder();
     
     
     /** Update the set of loggable inputs */
-    public void updateInputs(HopperIOInputs inputs) {
-   //     inputs.hopperMotor_Volts =  hopperMotor.getBusVoltage();
-        //hopperMotor.
-        
-    }
-    public double positionToRotations(double position){
-       return ((position * HopperConstants.HOPPER_WINCH_GEAR_RATIO) / HopperConstants.HOPPER_WINCH_CIRCUMFRENCE);
-
-    }
-  //  public void goToPosition(double position_inches){
-       // pidController.setSetpoint(positionToRotations(position_inches), SparkBase.ControlType.kMAXMotionPositionControl);
-   // }
     @Override
-    public void goToPosition(double position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'goToPosition'");
+    public void updateInputs(HopperIOInputs inputs) {
+        inputs.hopperMotor_Volts =  hopperMotor.getBusVoltage();
+        inputs.hopperMotor_Amps = hopperMotor.getOutputCurrent();
+        inputs.hopperExtension_Rotations = hopperEncoder.getPosition();
+        inputs.hopperExtension_Inches = inputs.hopperExtension_Rotations / HopperConstants.HOPPER_POSITION_TO_ANGLE_CONVERSION;
     }
    
+    @Override
+    public void goToPosition(double position_inches){
+        pidController.setSetpoint(position_inches * HopperConstants.HOPPER_POSITION_TO_ANGLE_CONVERSION, SparkBase.ControlType.kMAXMotionPositionControl);
+    }
+    public boolean atSetpoint(){
+        return pidController.isAtSetpoint();
+    }
 }
