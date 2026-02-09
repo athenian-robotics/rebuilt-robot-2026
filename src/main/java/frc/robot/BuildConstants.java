622ada80f4e675a11cdd6b1ff6d11c95b2bb1b53
Saathvik Kooturu <kooturusaathvik@gmail.com>
2026-02-09 12:30:34 -0800

    did research on hopper sim, differentiated it from HopperIOSparkMax

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 87;
-  public static final String GIT_SHA = "ec303d7a29d099532d6bc344748ac78812aaf18d";
-  public static final String GIT_DATE = "2026-02-08 14:44:46 EST";
+  public static final int GIT_REVISION = 88;
+  public static final String GIT_SHA = "268277cf055e844220d63d466c3fbbe6fdcb8d70";
+  public static final String GIT_DATE = "2026-02-08 15:54:07 EST";
   public static final String GIT_BRANCH = "HopperSIM";
-  public static final String BUILD_DATE = "2026-02-08 15:51:06 EST";
-  public static final long BUILD_UNIX_TIME = 1770583866812L;
+  public static final String BUILD_DATE = "2026-02-09 14:52:39 EST";
+  public static final long BUILD_UNIX_TIME = 1770666759969L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
@@ -2,16 +2,17 @@ package frc.robot.subsystems.hopper;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.sim.SparkRelativeEncoderSim;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 
 import frc.robot.Constants.HopperConstants;
 
-// TODO fully implement class
 public class HopperIOSim implements HopperIO {
     
     DCMotor m = DCMotor.getNeo550(1);
@@ -19,24 +20,26 @@ public class HopperIOSim implements HopperIO {
     
     private final SparkMax hopperMotor = new SparkMax(HopperConstants.SPARK_ID, MotorType.kBrushless);
     private final SparkMaxSim simMotor = new SparkMaxSim(hopperMotor, m);
-    private final SparkClosedLoopController pidController = hopperMotor.getClosedLoopController();
-    private final RelativeEncoder hopperEncoder = hopperMotor.getEncoder();
+    private final PIDController pidController = new PIDController(0, 0, 0);
+    private final SparkRelativeEncoderSim simEncoder = simMotor.getRelativeEncoderSim();
+    
     
     
     /** Update the set of loggable inputs */
     @Override
     public void updateInputs(HopperIOInputs inputs) {
-        inputs.hopperMotor_Volts =  hopperMotor.getBusVoltage();
-        inputs.hopperMotor_Amps = hopperMotor.getOutputCurrent();
-        inputs.hopperExtension_Rotations = hopperEncoder.getPosition();
+        inputs.hopperMotor_Volts =  simMotor.getBusVoltage();
+        inputs.hopperMotor_Amps = simMotor.getMotorCurrent();
+        inputs.hopperExtension_Rotations = simEncoder.getPosition();
         inputs.hopperExtension_Inches = inputs.hopperExtension_Rotations / HopperConstants.HOPPER_POSITION_TO_ANGLE_CONVERSION;
     }
    
     @Override
     public void goToPosition(double position_inches){
-        pidController.setSetpoint(position_inches * HopperConstants.HOPPER_POSITION_TO_ANGLE_CONVERSION, SparkBase.ControlType.kMAXMotionPositionControl);
+        double voltage = pidController.calculate(simEncoder.getPosition(), position_inches * HopperConstants.HOPPER_POSITION_TO_ANGLE_CONVERSION);
+        simMotor.setBusVoltage(voltage);
     }
     public boolean atSetpoint(){
-        return pidController.isAtSetpoint();
+        return pidController.atSetpoint();
     }
 }
