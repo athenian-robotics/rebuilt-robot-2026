86fc1b881a6c4f1bdbc0520a8640da53769e7a5b
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2026-02-16 13:50:43 -0800

    new build constants i guess

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 116;
-  public static final String GIT_SHA = "d5cd05fa81295750ae3a719f6b943093c79bd3e7";
-  public static final String GIT_DATE = "2026-02-16 16:44:02 EST";
+  public static final int GIT_REVISION = 132;
+  public static final String GIT_SHA = "2844611d654d003394f5f9747b87b816d20b4e2b";
+  public static final String GIT_DATE = "2026-02-16 16:50:03 EST";
   public static final String GIT_BRANCH = "merge-path-things-from-template";
-  public static final String BUILD_DATE = "2026-02-16 16:48:34 EST";
-  public static final long BUILD_UNIX_TIME = 1771278514236L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-16 16:50:27 EST";
+  public static final long BUILD_UNIX_TIME = 1771278627837L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
