6dcb330eab93b9cf60646cbe966eb89562265353
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2026-02-21 14:52:21 -0800

    Merged in from arc-template

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 143;
-  public static final String GIT_SHA = "f0c338113ab513a09e1b3b558fa14442295b831d";
-  public static final String GIT_DATE = "2026-02-21 17:43:44 EST";
-  public static final String GIT_BRANCH = "real-testing";
-  public static final String BUILD_DATE = "2026-02-21 17:43:58 EST";
-  public static final long BUILD_UNIX_TIME = 1771713838606L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 144;
+  public static final String GIT_SHA = "a6d519793599d90f686a5f5961800577dc0ea4e4";
+  public static final String GIT_DATE = "2026-02-21 17:44:09 EST";
+  public static final String GIT_BRANCH = "stuff-from-arc-template";
+  public static final String BUILD_DATE = "2026-02-21 17:51:34 EST";
+  public static final long BUILD_UNIX_TIME = 1771714294440L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
