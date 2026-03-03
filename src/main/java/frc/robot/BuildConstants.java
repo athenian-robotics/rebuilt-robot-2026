e7c3aa3fce8e100e8dfbe99a2057ae00478c5a65
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2026-03-02 16:16:10 -0800

    things

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 205;
-  public static final String GIT_SHA = "e7b712f4819bc4ce5f5ed53b780f3de04a1799c0";
-  public static final String GIT_DATE = "2026-03-01 18:56:53 EST";
-  public static final String GIT_BRANCH = "real-testing";
-  public static final String BUILD_DATE = "2026-03-02 19:02:34 EST";
-  public static final long BUILD_UNIX_TIME = 1772496154873L;
+  public static final int GIT_REVISION = 206;
+  public static final String GIT_SHA = "aebb789158a07e13c428fee175acb8b14ef4af3a";
+  public static final String GIT_DATE = "2026-03-02 19:06:03 EST";
+  public static final String GIT_BRANCH = "real-testing-new-motors";
+  public static final String BUILD_DATE = "2026-03-02 19:16:03 EST";
+  public static final long BUILD_UNIX_TIME = 1772496963605L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
