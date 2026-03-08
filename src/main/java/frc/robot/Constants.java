599bdfb32026330c7af341c6a4f81a2acbfb2ee3
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2026-03-08 11:21:24 -0700

    increase middle wheel and flywheel voltages

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -153,11 +153,11 @@ public class Constants {
     public static final int STAR_WHEEL_MOTOR = 23;
     public static final int ANGLE_CHANGER_MOTOR = 50;
 
-    public static final double MIDDLE_WHEEL_TO_SHOOTER_VOLTS = 6;
+    public static final double MIDDLE_WHEEL_TO_SHOOTER_VOLTS = 10;
     public static final double MIDDLE_WHEEL_TO_GROUND_VOLTS = -3;
     public static final double STAR_WHEEL_TO_GROUND_VOLTS = 7;
     public static final double STAR_WHEEL_TO_SHOOTER_VOLTS = 7;
-    public static final double FLYWHEEL_VOLTS = 9;
+    public static final double FLYWHEEL_VOLTS = 12;
 
     public static final double FLYWHEEL_KS = 0.0;
     public static final double FLYWHEEL_KV = 0.0;
