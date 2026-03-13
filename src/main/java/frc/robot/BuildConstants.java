b6b4934b4c5600256c89669edec8c0a67909b5de
28dchen <28dchen@athenian.org>
2026-03-13 16:40:37 -0700

    yay! limelight issue solved

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 238;
-  public static final String GIT_SHA = "d4debdc1adb8a38b211cbefb019821027484be4c";
-  public static final String GIT_DATE = "2026-03-10 20:06:09 EDT";
-  public static final String GIT_BRANCH = "real-testing";
-  public static final String BUILD_DATE = "2026-03-10 20:21:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1773188518677L;
+  public static final int GIT_REVISION = 241;
+  public static final String GIT_SHA = "0224bbccd20aac9312382660d4eb5427b3bc3da2";
+  public static final String GIT_DATE = "2026-03-10 21:07:22 EDT";
+  public static final String GIT_BRANCH = "limelight-issue-fix";
+  public static final String BUILD_DATE = "2026-03-13 19:34:45 EDT";
+  public static final long BUILD_UNIX_TIME = 1773444885389L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
