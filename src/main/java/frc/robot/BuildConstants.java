ffe9548e22de43f43bd1bb089de4ed1cff176eab
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2026-04-03 19:32:59 -0700

    19:33 pre quals 52

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 305;
-  public static final String GIT_SHA = "49e5f8d6d73464907e6e266503a85d794e28ff57";
-  public static final String GIT_DATE = "2026-04-03 21:33:35 EDT";
+  public static final int GIT_REVISION = 306;
+  public static final String GIT_SHA = "a10d6c976aca4543d96a80e52e69fe94a775f96a";
+  public static final String GIT_DATE = "2026-04-03 22:27:49 EDT";
   public static final String GIT_BRANCH = "competition";
-  public static final String BUILD_DATE = "2026-04-03 22:26:50 EDT";
-  public static final long BUILD_UNIX_TIME = 1775269610919L;
+  public static final String BUILD_DATE = "2026-04-03 22:32:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1775269938111L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
